85a7dadc9c9fa7629689b7df5a90e1597d330aca
Lucien <1155064923@link.cuhk.edu.hk>
2020-03-10 03:38:10 +0800

    drafted entry file for known victim training

---
+++ main_known_victim.py
@@ -0,0 +1,38 @@
+from coach_known_victim import CoachKnownVictim
+from othello.OthelloGame import OthelloGame as Game
+from othello.pytorch.NNet import NNetWrapper as nn
+from utils import *
+
+args = dotdict({
+    'numIters': 1000,
+    'numEps': 100,              # Number of complete self-play games to simulate during a new iteration.
+    'tempThreshold': 15,        #
+    'updateThreshold': 0.6,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
+    'maxlenOfQueue': 200000,    # Number of game examples to train the neural networks.
+    'numMCTSSims': 25,          # Number of games moves for MCTS to simulate.
+    'arenaCompare': 40,         # Number of games to play during arena play to determine if new net will be accepted.
+    'cpuct': 1,
+
+    'checkpoint': './temp/',
+    'load_model': False,
+    'load_folder_file': ('/dev/models/8x100x50','best.pth.tar'),
+    'victim_file': ('pretrained_models/othello/pytorch','6x6_153checkpoints_best.pth.tar'),
+    'numItersForTrainExamplesHistory': 20,
+
+})
+
+if __name__ == "__main__":
+    g = Game(6)
+    nnet = nn(g)
+    victim_net = nn(g)
+
+    if args.load_model:
+        nnet.load_checkpoint(args.load_folder_file[0], args.load_folder_file[1])
+
+    victim_net.load_checkpoint(args.victim_file[0], args.victim_file[1])
+
+    c = CoachKnownVictim(g, nnet, args)
+    if args.load_model:
+        print("Load trainExamples from file")
+        c.loadTrainExamples()
+    c.learn()
